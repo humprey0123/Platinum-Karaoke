e6cf1620eb534960f7e2574ac38839d5340d8613
Ian Humprey <ian.humprey09@gmail.com>
2026-07-04 16:34:18 +0800

    Settings Icon

--- app/src/main/java/com/example/platinumkaraoke/MainActivity.kt
+++ app/src/main/java/com/example/platinumkaraoke/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.platinumkaraoke
 
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
+import android.view.View
 
 /**
  * Loads [MainFragment].
@@ -10,6 +11,10 @@ class MainActivity : FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        val bg = findViewById<View>(R.id.dynamic_background)
+        bg.setBackgroundResource(R.drawable.bg_home)
     }
 }
