41218f51b7e40420a24ef9e189b914896e5db94a
Ian Humprey <ian.humprey09@gmail.com>
2026-07-07 14:37:31 +0800

    Dynamic Pages

--- app/src/main/java/com/example/platinumkaraoke/MainActivity.kt
+++ app/src/main/java/com/example/platinumkaraoke/MainActivity.kt
@@ -1,20 +1,50 @@
 package com.example.platinumkaraoke
 
+//import android.media.Image
 import android.os.Bundle
-import androidx.fragment.app.FragmentActivity
 import android.widget.ImageView
+import androidx.fragment.app.FragmentActivity
+import android.view.View
+
 
 /**
- * Loads [MainFragment].
  */
 class MainActivity : FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
 
-        val bg = findViewById<ImageView>(R.id.dynamic_background)
-        bg.setImageResource(R.drawable.bg_home)
+
+        findViewById<View>(R.id.nav_home).setOnClickListener {
+            showHome()
+        }
+
+        findViewById<View>(R.id.nav_search).setOnClickListener {
+            showSearch()
+        }
+
+        if (savedInstanceState == null) {
+            showHome()
+        }
     }
+
+    fun showHome() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_content, HomeFragment())
+            .commit()
+
+        findViewById<ImageView>(R.id.bg)
+            .setImageResource(R.drawable.bg_home)
+    }
+
+    fun showSearch() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_content, SearchFragment())
+            .commit()
+
+        findViewById<ImageView>(R.id.bg)
+            .setImageResource(R.drawable.bg_songlist)
+    }
+
 }
