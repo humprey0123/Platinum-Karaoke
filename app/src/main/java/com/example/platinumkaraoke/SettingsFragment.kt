4e31f8b4f1505c229eb6fe4450932e577741d9b7
Ian Humprey <ian.humprey09@gmail.com>
2026-08-11 14:43:13 +0800

    feat: working Authentication & Sound Fragment

--- app/src/main/java/com/example/platinumkaraoke/SettingsFragment.kt
+++ app/src/main/java/com/example/platinumkaraoke/SettingsFragment.kt
@@ -15,4 +15,36 @@ class SettingsFragment : Fragment(){
     ): View? {
         return inflater.inflate(R.layout.fragment_settings, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val navAuthentication = view.findViewById<View>(R.id.nav_authentication)
+        val main = view.findViewById<View>(R.id.fragment_settings)
+        val navSound = view.findViewById<View>(R.id.nav_sound)
+        val navEqualizer = view.findViewById<View>(R.id.nav_equalizer)
+
+        main.requestFocus()
+
+        navAuthentication.setOnClickListener {
+            replaceSettingsContent(SettingsAuthenticationFragment())
+        }
+
+        navSound.setOnClickListener {
+            replaceSettingsContent(SettingsSoundFragment())
+        }
+
+//        navEqualizer.setOnClickListener {
+//            replaceSettingsContent(EqualizerFragment())
+//        }
+
+        // Show Authentication by default
+        replaceSettingsContent(SettingsAuthenticationFragment())
+    }
+
+    private fun replaceSettingsContent(fragment: Fragment) {
+        childFragmentManager.beginTransaction()
+            .replace(R.id.settings_content, fragment)
+            .commit()
+    }
 }
---
+++ app/src/main/java/com/example/platinumkaraoke/SettingsSoundFragment.kt
@@ -0,0 +1,27 @@
+package com.example.platinumkaraoke
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class SettingsSoundFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(
+            R.layout.fragment_settings_sound,
+            container,
+            false
+        )
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val main = view.findViewById<View>(R.id.fragment_settings_sound)
+        main.requestFocus()
+    }
+}
