face7b8b3f101b81a57ec7c83dffdb40070920ce
Ian Humprey <ian.humprey09@gmail.com>
2026-08-13 12:38:23 +0800

    Display page with default and working selector

--- app/src/main/java/com/example/platinumkaraoke/SettingsScoreFragment.kt
+++ app/src/main/java/com/example/platinumkaraoke/SettingsScoreFragment.kt
@@ -23,5 +23,68 @@ class SettingsScoreFragment : Fragment() {
         val score = view.findViewById<View>(R.id.fragment_settings_score)
 
         score.requestFocus()
+
+        val scoreLevelOff = view.findViewById<View>(R.id.score_level_off)
+        val scoreLevelCasual = view.findViewById<View>(R.id.score_level_casual)
+        val scoreLevelExpert = view.findViewById<View>(R.id.score_level_expert)
+        val scoreLevelPro = view.findViewById<View>(R.id.score_level_pro)
+
+        val scoreAnimationDefault = view.findViewById<View>(R.id.score_animation_default)
+        val scoreAnimationCustom = view.findViewById<View>(R.id.score_animation_custom)
+
+        scoreLevelOff.setOnClickListener {
+            setSelectedScoreLevel(scoreLevelOff)
+        }
+        scoreLevelCasual.setOnClickListener {
+            setSelectedScoreLevel(scoreLevelCasual)
+        }
+        scoreLevelExpert.setOnClickListener {
+            setSelectedScoreLevel(scoreLevelExpert)
+        }
+        scoreLevelPro.setOnClickListener {
+            setSelectedScoreLevel(scoreLevelPro)
+        }
+
+        scoreAnimationDefault.setOnClickListener {
+            setSelectedScoreAnimation(scoreAnimationDefault)
+        }
+        scoreAnimationCustom.setOnClickListener {
+            setSelectedScoreAnimation(scoreAnimationCustom)
+        }
+
+        setSelectedScoreAnimation(scoreAnimationDefault)
+        setSelectedScoreLevel(scoreLevelCasual)
+    }
+
+    private fun setSelectedScoreLevel (selectedView: View) {
+        selectedView.isSelected = true
+
+        val levelViews = listOf(
+            view?.findViewById<View>(R.id.score_level_off),
+            view?.findViewById<View>(R.id.score_level_casual),
+            view?.findViewById<View>(R.id.score_level_expert),
+            view?.findViewById<View>(R.id.score_level_pro),
+        )
+
+        levelViews.forEach { nav ->
+            if (nav != selectedView) {
+                nav?.isSelected = false
+            }
+        }
+    }
+
+    private fun setSelectedScoreAnimation (selectedView: View) {
+        selectedView.isSelected = true
+
+        val levelViews = listOf(
+            view?.findViewById<View>(R.id.score_animation_custom),
+            view?.findViewById<View>(R.id.score_animation_default)
+        )
+
+        levelViews.forEach { nav ->
+            if (nav != selectedView) {
+                nav?.isSelected = false
+            }
+        }
     }
 }
