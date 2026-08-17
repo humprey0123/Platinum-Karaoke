abadb07819cccea9608b1db3ba62980276df6374
Ian Humprey <ian.humprey09@gmail.com>
2026-08-17 16:57:55 +0800

    Password page and update Settings Update page

--- app/src/main/java/com/example/platinumkaraoke/SettingsMicFragment.kt
+++ app/src/main/java/com/example/platinumkaraoke/SettingsMicFragment.kt
@@ -23,42 +23,5 @@ class SettingsMicFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val micFragment = view.findViewById<View>(R.id.fragment_settings_mic_settings)
         micFragment.requestFocus()
-
-        // Song
-        setupGroupSelection(
-            listOf(R.id.song_usb, R.id.song_network),
-            R.id.song_usb
-        )
-
-        // System Upgrade
-        setupGroupSelection(
-            listOf(R.id.system_upgrade_usb, R.id.system_upgrade_network),
-            R.id.system_upgrade_usb
-        )
-
-        // Auto OTA
-        setupGroupSelection(
-            listOf(R.id.auto_ota_disable, R.id.auto_ota_Enable),
-            R.id.auto_ota_disable
-        )
-    }
-
-    private fun setupGroupSelection(viewIds: List<Int>, defaultId: Int) {
-        val views = viewIds.mapNotNull { view?.findViewById<View>(it) }
-
-        views.forEach { v ->
-            v.setOnClickListener {
-                updateSelection(views, it)
-            }
-        }
-
-        // Set initial selection
-        view?.findViewById<View>(defaultId)?.let {
-            updateSelection(views, it)
-        }
-    }
-
-    private fun updateSelection(group: List<View>, selectedView: View) {
-        group.forEach { it.isSelected = (it == selectedView) }
     }
 }
